0241a47c9ff2057c407897c6ef9667dac816f03d
wkdalswn11 <wkdalswn133@naver.com>
2021-01-08 09:12:56 +0900

    20210107

---
+++ src/main/java/org/zerock/mapper/TimeMapper.java
@@ -0,0 +1,22 @@
+package org.zerock.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+	
+	@Select("SELECT sysdate FROM dual")
+	public String getTime();
+	
+	
+	public String getTime2();
+}
+
+// 	클래스 로딩
+// 	커넥션 열고
+// 	statement 얻고
+// 	query 실행
+
+//	SELECT sysdate FROM dual
+
+// 	resultSet처리
+//	커넥션 닫고
